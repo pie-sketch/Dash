d4257cad03f522547d4a0828e7d3a3d6104a3f19
pie-sketch <mgeodesics@gmail.com>
2025-07-23 15:32:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -183,4 +183,5 @@ def toggle_previous(n, is_open):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run_server(host="0.0.0.0", port=port, debug=False)
+
