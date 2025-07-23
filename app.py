ce5e13bb21422f764934e83d2d203ec4fc036e19
pie-sketch <mgeodesics@gmail.com>
2025-07-23 12:54:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -157,4 +157,5 @@ def update_dashboard(n):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False)
+
